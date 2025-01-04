b73f8515a5269bb656731e615006b6238b60ec98
rifat32 <rifatbilalphilips@gmail.com>
2025-01-04 22:27:22 +0600

    subscription fixed

--- app/Http/Controllers/CustomWebhookController.php
+++ app/Http/Controllers/CustomWebhookController.php
@@ -73,7 +73,11 @@ class CustomWebhookController extends WebhookController
     {
 
         // Extract required data from payment charge
-        $amount = $data['amount_total'] ?? null;
+
+        $amount = isset($data['amount_total'])
+            ? $data['amount_total'] / 100 // Amount in dollars
+            : null;
+
         $customerID = $data['customer'] ?? null;
         $metadata = $data["metadata"] ?? [];
         // Add more fields as needed
@@ -85,33 +89,32 @@ class CustomWebhookController extends WebhookController
         $user = User::where("stripe_id", $customerID)->first();
 
 
-        if(!empty($metadata["service_plan_id"])) {
+        if (!empty($metadata["service_plan_id"])) {
             $service_plan = ServicePlan::find($metadata["service_plan_id"]);
         } else {
             $service_plan = ServicePlan::find($user->business->service_plan_id);
         }
 
-      $subscription = BusinessSubscription::create([
+        $subscription = BusinessSubscription::create([
             'business_id' => $user->business->id,
             'service_plan_id' => $service_plan->id,
-            'start_date' => now(),  // Start date of the subscription
-            'end_date' => Carbon::now()->addDays($service_plan->duration_months * (env("IS_DEVELOPMENT_MODE") == "true" ? 1 : 30)),  // End date based on plan duration
-            'amount' => ($amount / 100),
+            'start_date' => "1970-01-01 00:00:00",
+            'end_date' => "1970-01-01 00:00:00",  // End date based on plan duration
+            'amount' => $amount,
             'paid_at' => now(),
             'transaction_id' => $data['id'],
 
         ]);
 
-            $reseller = $user->business->reseller;
+        $reseller = $user->business->reseller;
 
-            try {
-                Mail::to(['kids20acc@gmail.com', 'ralashwad@gmail.com', $reseller->email])->send(new UserRegistered($user,$subscription));
-            } catch (\Exception $e) {
-                // Log the error with stack trace for debugging
-                Log::error("Failed to send email: " . $e->getMessage(), ['exception' => $e]);
-                // Optionally, handle specific actions if email fails (e.g., notify admin)
-            }
-
+        try {
+            Mail::to(['kids20acc@gmail.com', 'ralashwad@gmail.com', $reseller->email])->send(new UserRegistered($user, $subscription));
+        } catch (\Exception $e) {
+            // Log the error with stack trace for debugging
+            Log::error("Failed to send email: " . $e->getMessage(), ['exception' => $e]);
+            // Optionally, handle specific actions if email fails (e.g., notify admin)
+        }
     }
 
     protected function handleSubscriptionPaymentSucceeded($invoice)
@@ -122,63 +125,49 @@ class CustomWebhookController extends WebhookController
             // This is a subscription payment
 
             // Extract required data from the invoice
-            $amount = isset($invoice['lines']['data'][0]['amount'])
-            ? $invoice['lines']['data'][0]['amount'] / 100 // Amount in dollars
-            : null;
+            $lastIndex = count($invoice['lines']['data']) - 1;
+            $amount = isset($invoice['lines']['data'][$lastIndex]['amount'])
+                ? $invoice['lines']['data'][$lastIndex]['amount'] / 100 // Amount in dollars
+                : null;
+
             $customerID = $invoice['customer'] ?? null; // Customer ID from Stripe
             $subscriptionID = $invoice['subscription'];  // Subscription ID
             $metadata = $invoice["subscription_details"]["metadata"] ?? []; // Metadata from the invoice
             $periodStart = isset($invoice['lines']['data'][0]['period']['start'])
-            ? Carbon::createFromTimestamp($invoice['lines']['data'][0]['period']['start'])
-            : null;
+                ? Carbon::createFromTimestamp($invoice['lines']['data'][0]['period']['start'])
+                : null;
 
-        $periodEnd = isset($invoice['lines']['data'][0]['period']['end'])
-            ? Carbon::createFromTimestamp($invoice['lines']['data'][0]['period']['end'])
-            : null;
+            $periodEnd = isset($invoice['lines']['data'][0]['period']['end'])
+                ? Carbon::createFromTimestamp($invoice['lines']['data'][0]['period']['end'])
+                : null;
 
             // Ensure that the URL in the metadata matches, if provided
             if (!empty($metadata["our_url"]) && $metadata["our_url"] != route('stripe.webhook')) {
                 return;
             }
 
-         // Fetch the user based on the Stripe customer ID
+            // Fetch the user based on the Stripe customer ID
             $user = User::where("stripe_id", $customerID)->first();
 
             if (!$user) {
                 // If the user does not exist, log the error and stop processing
                 Log::error("User not found for customer ID: $customerID");
                 return response()->json([
-                    "message"=> "User not found for customer ID: $customerID"
-                ],400);
+                    "message" => "User not found for customer ID: $customerID"
+                ], 400);
             }
 
             $service_plan = !empty($metadata["service_plan_id"])
-            ? ServicePlan::find($metadata["service_plan_id"])
-            : ServicePlan::find($user->business->service_plan_id);
+                ? ServicePlan::find($metadata["service_plan_id"])
+                : ServicePlan::find($user->business->service_plan_id);
 
-        if (!$service_plan) {
-            // If the service plan is not found, log the error and stop processing
-            Log::error("Service plan not found for user ID: $user->id");
-            return response()->json([
-                "message"=> "Service plan not found for user ID: $user->id"
-            ],400);
-
-        }
-
-
-
-
-
-            $subscription_count = BusinessSubscription::where([
-                'business_id' => $user->business->id
-            ])
-                ->count();
-                $reseller = $user->business->reseller;
-
-            if ($subscription_count >= 1) {
-
-                 // Create or update the business subscription
-
+            if (!$service_plan) {
+                // If the service plan is not found, log the error and stop processing
+                Log::error("Service plan not found for user ID: $user->id");
+                return response()->json([
+                    "message" => "Service plan not found for user ID: $user->id"
+                ], 400);
+            }
 
             $subscription = BusinessSubscription::create([
                 'business_id' => $user->business->id,
@@ -190,8 +179,15 @@ class CustomWebhookController extends WebhookController
                 'transaction_id' => $invoice['id'], // Transaction ID from Stripe
                 'subscription_id' => $subscriptionID // Store the subscription ID
             ]);
-            Log::info(json_encode($subscription));
-                // Send email
+
+            // Send email
+            $subscription_count = BusinessSubscription::where([
+                'business_id' => $user->business->id
+            ])
+                ->count();
+
+            if ($subscription_count > 2) {
+                $reseller = $user->business->reseller;
                 try {
                     Mail::to(['kids20acc@gmail.com', 'ralashwad@gmail.com', $reseller->email])->send(new UserSubscriptionRenewed($user, $subscription));
                 } catch (\Exception $e) {
@@ -199,11 +195,11 @@ class CustomWebhookController extends WebhookController
                     Log::error("Failed to send email: " . $e->getMessage(), ['exception' => $e]);
                 }
             }
-
         } else {
             // Not a subscription payment, handle other events (e.g., one-time payment)
             Log::warning("Received non-subscription payment event.");
         }
     }
+
 
 }
